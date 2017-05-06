f8ec58991d3a984a414de5fddcdbe0137bc90c3a
tuananhpham94 <tuananh191194@gmail.com>
2017-05-06 15:02:02 +1200

    database

--- config/database.php
+++ config/database.php
@@ -67,9 +67,9 @@ return [
         'mysql' => [
             'driver' => 'mysql',
             'host' => env('DB_HOST', 'localhost'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'database' => env('DB_DATABASE', 'test-blog'),
+            'username' => env('DB_USERNAME', 'root'),
+            'password' => env('DB_PASSWORD', '123456'),
             'port' => env('DB_PORT', 3306),
             'charset' => 'utf8',
             'collation' => 'utf8_unicode_ci',
